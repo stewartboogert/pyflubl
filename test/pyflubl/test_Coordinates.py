d3666e6f6a9f74ce4e2d08fded4d3d2365e1254f
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-02-23 23:44:45 +0000

    test: range of different tests

--- test/pyflubl/test_Coordinates.py
+++ test/pyflubl/test_Coordinates.py
@@ -32,7 +32,7 @@ def test_T001_drift_many_coordinates() :
 
 def test_T002_rbend_coordinates() :
     m = _pfbl.Builder.Machine()
-    e = m.AddRBend("rb1", length=1, angle=0.05)
+    e = m.AddRBend("rb1", length=1, angle=30/180.*_np.pi)
 
     c = _pfbl.Coordinates.Coordinates()
     c.Append(e)
@@ -42,9 +42,57 @@ def test_T002_rbend_coordinates() :
 
     return c
 
+def test_T002_rbend_many_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddRBend("rb1", length=1, angle=30/180.*_np.pi)
+    e2 = m.AddRBend("rb2", length=1, angle=30/180.*_np.pi)
+    e3 = m.AddRBend("rb3", length=1, angle=30/180.*_np.pi)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+    e7 = m.AddDrift("d4", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+    c.Append(e3)
+    c.Append(e7)
+
+    c.Build()
+    c.SaveJSON("T002_rbend_many_coordinates.json")
+
+    return c
+
+def test_T002_rbend_dogleg_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddRBend("rb1", length=1, angle=30/180.*_np.pi)
+    e2 = m.AddRBend("rb2", length=1, angle=-30/180.*_np.pi)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+
+    c.Build()
+    c.SaveJSON("T002_rbend_dogleg_coordinates.json")
+
+    return c
+
 def test_T002_rbend_tilt_coordinates() :
     m = _pfbl.Builder.Machine()
-    e = m.AddRBend("rb1", length=1, angle=0.05, tilt=_np.pi/2)
+    e = m.AddRBend("rb1", length=1, angle=30/180.*_np.pi, tilt=_np.pi/2)
 
     c = _pfbl.Coordinates.Coordinates()
     c.Append(e)
@@ -54,9 +102,35 @@ def test_T002_rbend_tilt_coordinates() :
 
     return c
 
+def test_T002_rbend_tilt_many_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddRBend("rb1", length=1, angle=30/180.*_np.pi, tilt=_np.pi/2)
+    e2 = m.AddRBend("rb2", length=1, angle=30/180.*_np.pi, tilt=_np.pi/2)
+    e3 = m.AddRBend("rb3", length=1, angle=30/180.*_np.pi, tilt=_np.pi/2)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+    e7 = m.AddDrift("d4", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+    c.Append(e3)
+    c.Append(e7)
+
+    c.Build()
+    c.SaveJSON("T002_rbend_tilt_many_coordinates.json")
+
+    return c
+
 def test_T003_sbend_coordinates() :
     m = _pfbl.Builder.Machine()
-    e = m.AddRBend("sb1", length=1, angle=0.05)
+    e = m.AddRBend("sb1", length=1, angle=30/180.*_np.pi)
 
     c = _pfbl.Coordinates.Coordinates()
     c.Append(e)
@@ -66,9 +140,83 @@ def test_T003_sbend_coordinates() :
 
     return c
 
+def test_T003_sbend_many_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddSBend("rb1", length=1, angle=30/180.*_np.pi)
+    e2 = m.AddSBend("rb2", length=1, angle=30/180.*_np.pi)
+    e3 = m.AddSBend("rb3", length=1, angle=30/180.*_np.pi)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+    e7 = m.AddDrift("d4", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+    c.Append(e3)
+    c.Append(e7)
+
+    c.Build()
+    c.SaveJSON("T003_sbend_many_coordinates.json")
+
+    return c
+
+def test_T003_sbend_dogleg_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddSBend("rb1", length=1, angle=30/180.*_np.pi)
+    e2 = m.AddSBend("rb2", length=1, angle=-30/180.*_np.pi)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+
+    c.Build()
+    c.SaveJSON("T003_sbend_dogleg_coordinates.json")
+
+    return c
+
+def test_T003_sbend_tilt_many_coordinates() :
+    m = _pfbl.Builder.Machine()
+    e1 = m.AddSBend("rb1", length=1, angle=29.999/180.*_np.pi, tilt=_np.pi/2)
+    e2 = m.AddSBend("rb2", length=1, angle=29.999/180.*_np.pi, tilt=_np.pi/2)
+    e3 = m.AddSBend("rb3", length=1, angle=29.999/180.*_np.pi, tilt=_np.pi/2)
+
+    e4 = m.AddDrift("d1", length=1)
+    e5 = m.AddDrift("d2", length=1)
+    e6 = m.AddDrift("d3", length=1)
+    e7 = m.AddDrift("d4", length=1)
+
+
+    c = _pfbl.Coordinates.Coordinates()
+    c.Append(e4)
+    c.Append(e1)
+    c.Append(e5)
+    c.Append(e2)
+    c.Append(e6)
+    c.Append(e3)
+    c.Append(e7)
+
+    c.Build()
+    c.SaveJSON("T003_sbend_tilt_many_coordinates.json")
+
+    return c
+
 def test_T003_sbend_tilt_coordinates() :
     m = _pfbl.Builder.Machine()
-    e = m.AddRBend("sb1", length=1, angle=0.05, tilt=_np.pi/2)
+    e = m.AddRBend("sb1", length=1, angle=30/180.*_np.pi, tilt=_np.pi/2)
 
     c = _pfbl.Coordinates.Coordinates()
     c.Append(e)
