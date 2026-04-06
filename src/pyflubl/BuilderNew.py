6ffc61924d7f146c5184d63161b4769c7f41859b
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-04-06 23:27:25 +0100

    pyflubl: add field for rbend

--- src/pyflubl/BuilderNew.py
+++ src/pyflubl/BuilderNew.py
@@ -988,6 +988,8 @@ class Machine(_Coordinates) :
             print("pyflubl.BuilderNew.Machine.MakeFlukaRBend: Making rbend element ", name)
 
         length = element.length*1000
+        angle = element["angle"]
+
         rotation, translation = self._MakeOffsetAndTiltTransforms(element, rotation, translation)
 
         outerHorizontalSize = element["outerHorizontalSize"]
@@ -1024,9 +1026,44 @@ class Machine(_Coordinates) :
 
         self._AddBookkeepingTransformation(name, rotation, translation, geomtranslation)
 
-        return self._MakeFlukaComponentCommonG4(name,outerLogical, outerPhysical, flukaConvert,
-                                                rotation, translation, geomtranslation, "rbend")
+        ret_dict = self._MakeFlukaComponentCommonG4(name,outerLogical, outerPhysical, flukaConvert,
+                                                    rotation, translation, geomtranslation, "rbend")
 
+        # calculate field strength
+        rho = length/(2*_np.sin(angle/2.))
+        b_field = self._CalculateDipoleFieldStrength(self.beam.momentum, rho)
+
+        # bookkeeping info for element
+        bki = self.elementBookkeeping[element.name]
+
+        # make field transform
+        translation = bki['translation']
+        rotation = _matrix2tbxyz(_np.array(bki['rotation']))
+        rdi = _rotoTranslationFromTra2("TM"+format(self.flukamgncount, "03"),[rotation, translation])
+        if len(rdi) > 0 :
+            self.flukaregistry.addRotoTranslation(rdi)
+
+        # find vacuum region
+        vacuum_index = bki['physicalVolumes'].index(vacPhysical.name)
+        vacuum_region = bki['flukaRegions'][vacuum_index]
+
+        # make and assign field to region(s)
+        mgnname = "MG"+format(self.flukamgncount, "03")
+        mgnfield = _Mgnfield(strength=-b_field,rotDefini=rdi.name, applyRegion=0,
+                             regionFrom=vacuum_region, regionTo=None, regionStep=None,
+                             sdum = mgnname)
+        self.AddMgnfield(mgnfield)
+
+        mgncreat = _Mgncreat(fieldType=_Mgncreat.DIPOLE, applicableRadius=0, xOffset=0, yOffset=0, mirrorSymmetry=0, sdum=mgnname)
+        self.AddMgncreat(mgncreat)
+
+        # add magnetic field to assimat
+        self.flukaregistry.assignmaAddMagnetic(vacuum_region, mgnname)
+
+        # increment mgn count
+        self.flukamgncount += 1
+
+        return ret_dict
 
     def MakeFlukaSBend(self,
                        name,
