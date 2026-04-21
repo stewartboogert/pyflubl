c87884856796a067e8a859b4e00621045badf235
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-04-21 13:08:07 +0100

    test: simple sbend with some scatting

--- test/pyflubl/test_T051_Lattice.py
+++ test/pyflubl/test_T051_Lattice.py
@@ -225,3 +225,74 @@ def make_T051_Lattice_SBend() :
 
 def test_T051_Lattice_SBend() :
     make_T051_Lattice_SBend()
+
+def make_T051_Lattice_SBend_simple() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.BuilderNew.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam(momentumOrKe=1, energySpread=0.0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+    m.AddBeam(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(1000)
+    m.AddStart(s)
+
+    uic = _pfbl.Fluka.Usricall()
+    m.AddUsricall(uic)
+
+    uoc = _pfbl.Fluka.Usrocall()
+    m.AddUsrocall(uoc)
+
+    ud = _pfbl.Fluka.Userdump(mgdraw=100,lun=23,mgdrawOption=-1,userDump=None, outputFile="dump")
+    m.AddUserdump(ud)
+
+    # set world material
+    m.world_material = "VACUUM"
+
+    d1 = m.AddDrift(name="d1",
+                    length=1.0,
+                    beampipeMaterial = "IRON",
+                    outerMaterial = "AIR",
+                    add=False)
+
+    sb1 = m.AddSBend(name="sb1",
+                     length=1.0,
+                     angle=_np.pi*5/180.0,
+                     add=False)
+
+    sb2 = m.AddSBend(name="sb2",
+                     length=1.0,
+                     angle=-_np.pi*5/180.0,
+                     add=False)
+
+    m.AddLatticePrototype(d1)
+    m.AddLatticePrototype(sb1)
+    m.AddLatticePrototype(sb2)
+
+    m.AddLatticeInstance("d1i1","d1")
+    m.AddRCol(name="c1",length=0.005,material="IRON",xsize=0,ysize=0)
+    m.AddLatticeInstance("d1i2","d1")
+    m.AddLatticeInstance("sb1i1","sb1")
+    m.AddLatticeInstance("d1i3","d1")
+    m.AddLatticeInstance("sb2i1","sb2")
+    m.AddLatticeInstance("d1i4","d1")
+
+    m.AddSamplerPlane(name="s1", length=1e-4)
+
+    m.Write(this_dir+"/T051_Lattice_SBend_simple")
+
+    return m
+
+def test_T051_Lattice_SBend() :
+    make_T051_Lattice_SBend_simple()
