ff6e3253e6638a4b03696304195550810dd52450
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-04-21 22:29:59 +0100

    util: proper opening of bk file

--- src/pyflubl/Utilities.py
+++ src/pyflubl/Utilities.py
@@ -3,12 +3,9 @@ import json as _json
 def load_bookkeeping(bookkeeping_file):
 
     # open file
-    f = open(bookkeeping_file,"r")
+    with open(bookkeeping_file, "r", encoding="utf-8") as f:
 
-    # load JSON data
-    d = _json.load(f)
+        # load JSON data
+        d = _json.load(f)
 
-    # close file
-    f.close()
-
-    return d
+        return d
