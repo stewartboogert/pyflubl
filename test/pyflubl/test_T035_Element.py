0afb913d39798cbc4c6e373f10b595f55bc406eb
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-04-23 22:09:50 +0100

    test: BDSIM RBend test

--- test/pyflubl/test_T035_Element.py
+++ test/pyflubl/test_T035_Element.py
@@ -235,6 +235,35 @@ def make_T035_Element_Fluka_RBend() :
 def test_T035_Element_Fluka_RBend() :
     make_T035_Element_Fluka_RBend()
 
+def make_T035_Element_BDSIM_RBend() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.BuilderNew.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam(momentumOrKe=1, energySpread=0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+
+    m.AddBeam(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(10)
+    m.AddStart(s)
+
+    m.AddCustomG4File(name="rb1",
+                      length=1.5,
+                      geometryFile=this_dir+"/../bdsim/02_rbend.gdml",
+                      lvName="el_centre_container_lv0x60000270fa20")
+
+    m.Write(this_dir+"/T035_Element_BDSIM_RBend")
 
 if __name__ == "__main__":
     test_T035_Element_G4()
