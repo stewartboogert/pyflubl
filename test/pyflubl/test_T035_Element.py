cb4b24604efe0fa9137aed7e1b8f1e1849e4261d
Stewart Boogert <stewart.boogert@cockcroft.ac.uk>
2026-04-24 21:39:36 +0100

    test: G4 file instance

--- test/pyflubl/test_T035_Element.py
+++ test/pyflubl/test_T035_Element.py
@@ -268,6 +268,39 @@ def make_T035_Element_BDSIM_RBend() :
 def test_T035_Element_BDSIM_RBend() :
     make_T035_Element_BDSIM_RBend()
 
+def make_T035_ELement_BDSIM_Quad() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.BuilderNew.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam(momentumOrKe=1, energySpread=0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+
+    m.AddBeam(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(10)
+    m.AddStart(s)
+
+    m.AddCustomG4File(name="q1",
+                      length=0.25,
+                      geometryFile=this_dir+"/../bdsim/04_quad.gdml",
+                      lvName="el_container_lv0x6000015d3340")
+
+    m.Write(this_dir+"/T035_Element_BDSIM_Quad")
+
+def test_T035_Element_BDSIM_Quad() :
+    make_T035_ELement_BDSIM_Quad()
+
 if __name__ == "__main__":
     test_T035_Element_G4()
     test_T035_Element_G4_File()
--- test/pyflubl/test_T051_Lattice.py
+++ test/pyflubl/test_T051_Lattice.py
@@ -581,3 +581,59 @@ def make_T051_Lattice_CustomG4File_RBend() :
 
 def test_T051_Lattice_CustomG4File_RBend() :
     make_T051_Lattice_CustomG4File_RBend()
+
+def make_T051_Lattice_CustomG4File_Quad() :
+    this_dir = _os.path.dirname(_os.path.abspath(__file__))
+
+    m = _pfbl.BuilderNew.Machine(bakeTransforms=True)
+
+    d = _pfbl.Fluka.Defaults('EM-CASCA')
+    m.AddDefaults(d)
+
+    b = _pfbl.Fluka.Beam(momentumOrKe=1, energySpread=0.0, sdum="ELECTRON")
+    bp = _pfbl.Fluka.Beampos(xCentre=0, yCentre=0, zCentre=0, xCosine=0, yCosine=0)
+    ba = _pfbl.Fluka.BeamAxes(xxCosine=1, xyCosine=0, xzCosine=0,
+                              zxCosine=0, zyCosine=0, zzCosine=1)
+    m.AddBeam(b)
+    m.AddBeampos(bp)
+    m.AddBeamaxes(ba)
+
+    r = _pfbl.Fluka.Randomiz()
+    m.AddRandomiz(r)
+
+    s = _pfbl.Fluka.Start(10)
+    m.AddStart(s)
+
+    uic = _pfbl.Fluka.Usricall()
+    m.AddUsricall(uic)
+
+    uoc = _pfbl.Fluka.Usrocall()
+    m.AddUsrocall(uoc)
+
+    ud = _pfbl.Fluka.Userdump(mgdraw=100,lun=23,mgdrawOption=-1,userDump=None, outputFile="dump")
+    m.AddUserdump(ud)
+
+    # set world material
+    m.world_material = "VACUUM"
+
+    g4 = m.AddCustomG4File(name="g4",
+                           length=0.25,
+                           geometryFile=this_dir+"/../bdsim/04_quad.gdml",
+                           lvName="el_container_lv0x6000015d3340",
+                           add=False)
+
+    m.AddLatticePrototype(g4)
+
+    m.AddDrift(name="d1",length=1)
+    m.AddLatticeInstance("g4i1","g4")
+    m.AddDrift(name="d2",length=1)
+    m.AddLatticeInstance("g4i2","g4")
+    m.AddDrift(name="d3",length=1)
+    m.AddLatticeInstance("g4i3","g4")
+
+    m.Write(this_dir+"/T051_Lattice_CustomG4File_Quad")
+
+    return m
+
+def test_T051_Lattice_CustomG4File_Quad() :
+    make_T051_Lattice_CustomG4File_Quad()
